e969002998bc8d8c57820594935199b9cd103d4c
delorayn1 <delorayn1@gmail.com>
2014-08-09 10:39:27 +0200

    A test software for B/W antialiasing

---
+++ antialiasing/antialiasing_test.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image as img
+
+n_antialias = 1 #Aliasing setting
+antialias_weigh = 5
+
+def get_coords(x, y, max_col, max_row, n):
+	n_range = 2*n+1
+	coord_list = []
+	for i in range(0, n_range):
+		for j in range(0, n_range):
+			x_dim = x+i-n
+			y_dim = y+j-n
+
+			x_dim = 0 if x_dim<0 else x_dim
+			y_dim = 0 if y_dim<0 else y_dim
+
+			x_dim = max_col-1 if x_dim>=max_col else x_dim
+			y_dim = max_row-1 if y_dim>=max_row else y_dim
+
+			coord_list.append((x_dim, y_dim))
+
+	return set(coord_list)
+
+im = img.open("test.jpg")
+im = im.convert('L')
+img_array = np.array(im)
+
+
+nrows = len(img_array)
+ncols = len(img_array[0])
+
+antialiased_img = np.zeros(shape=(nrows,ncols))
+
+for row in range(0, nrows):
+	for col in range (0, ncols):
+		s = 0
+		n_size = (2*n_antialias+1)**2
+		for x, y in get_coords(row, col, nrows, ncols, n_antialias):
+			s += img_array[x][y]
+		avg_val = (s+antialias_weigh*img_array[row][col]) / (n_size+antialias_weigh)
+		antialiased_img[row][col] = avg_val
+
+						
+
+
+
+
+im = img.fromarray(antialiased_img)
+im = im.convert('L')
+#im.show()
+im.save("antialiasing_test.jpg")
