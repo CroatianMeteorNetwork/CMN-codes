737d516e7cd10a4a9348e62591114fc2b7ec47d5
delorayn1 <delorayn1@gmail.com>
2014-10-10 09:58:44 +0200

    Added chunk calculation (mean)

--- HMM_radio2txt/HMM_radio2txt.py
+++ HMM_radio2txt/HMM_radio2txt.py
@@ -1,17 +1,25 @@
 from scipy.io.wavfile import read
-import datetime
+import numpy as np
+import datetime, math
 
+numchunks = 6000
 file_name = "RAD_BEDOUR_20111007_0135_BEUCCL_SYS001.wav" #WAV source file name
+
+
+
 
 samprate, wavdata = read(file_name) #Get data from WAV file (samprate = samples/sec), wavdata contains raw levels data
 
 
+chunks = np.array_split(wavdata, numchunks) #Split array into chunks
+dbs = [np.mean(chunk) for chunk in chunks] #Calculate dB values from the mean of the chunks
+
 print samprate
 
 data_file = open('wav_data.txt', 'w')
-data_file.write('Sample rate: '+str(samprate)+' samples/sec\n')
+data_file.write('Sample rate: '+str(samprate)+' samples/sec reduced to '+str(numchunks)+' chunks\n')
 
-for no, line in enumerate(wavdata):
+for no, line in enumerate(dbs):
 	data_file.write(str(no+1)+' '+str(line)+'\n')
 
 data_file.close()
