3b1ed9aee0eb362dcc58ba9a1bba860f0fbffb71
delorayn1 <delorayn1@gmail.com>
2014-12-16 18:01:23 +0100

    Added license

--- FF_bin_suite/FF_bin_suite.py
+++ FF_bin_suite/FF_bin_suite.py
@@ -1,4 +1,19 @@
 # coding=utf-8
+# Copyright 2014 Denis Vida, denis.vida@gmail.com
+
+# The FF_bin_suite is free software; you can redistribute
+# it and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, version 2.
+
+# The FF_bin_suite is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with the Astrometry.net suite ; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
 """Module for handling FF*.bin files produced by CAMS software.
 Author: Denis Vida
 
