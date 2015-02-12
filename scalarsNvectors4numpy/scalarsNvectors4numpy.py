07cc68a5ea5add1eca8b2d0841d4518edb22717d
dvida <denis.vida@gmail.com>
2015-02-12 18:17:03 +0100

    Decotration now works for only one function

--- scalarsNvectors4numpy/scalarsNvectors4numpy.py
+++ scalarsNvectors4numpy/scalarsNvectors4numpy.py
@@ -18,76 +18,73 @@
 import numpy as np
 
 
+def universalDecorator(func):
+    """ Checks the first argument of a function, if it is an array_like, call the given function. 
+        Otherwise if it is a scalar, converts it to a list. If it is none of the above, return [].
+    """
+
+    def _replaceArgument(args, argument):
+        arg_list = list(args)
+        arg_list.pop(0)
+        arg_list.insert(0, argument)
+        args = tuple(arg_list)
+
+        return args
+
+
+    def _inner(*args, **kwargs):
+
+        argument = args[0]
+
+        if isinstance(argument, (frozenset, set)):
+            # Convert set to list
+            argument = list(argument)
+            args = _replaceArgument(args, argument)
+
+        elif np.isscalar(argument):
+            # Convert scalar to list
+            argument = [argument]
+            args = _replaceArgument(args, argument)
+
+
+        if isinstance(argument, (np.ndarray, list, tuple)):
+            return func(*args, **kwargs)
+        else:
+            return np.array([])
+
+    return _inner
+
+
+@universalDecorator
 def sinList(x):
-	""" Take sine of a list and cut the list valies that are below 0.
-	"""
-	res = np.sin(x)
-	res[res<0] = 0
+    """ Take sine of a list and cut the list valies that are below 0.
+    """
+    res = np.sin(x)
+    res[res<0] = 0
 
-	return res
-
-
-def sinScalar(x):
-	""" Take a sine of a scalar and cut the value to 0 if the sine is < 0.
-	"""
-	res = np.sin(x)
-	if res < 0: 
-		res = 0
-
-	return np.array(res, copy=False, ndmin=1)
-
-
-def universalDecorator(func, alt_func):
-	""" Checks the first argument of a function, if it is an array_like, call the first function. 
-		Otherwise if it is a scalar, call the second function. If it is none of the above, return [].
-	"""
-
-	def _inner(*args, **kwargs):
-		# Check if first argument is an array_like or a scalar
-		argument = args[0]
-
-		# Convert set to list
-		if isinstance(argument, (frozenset, set)):
-			argument = list(argument)
-			arg_list = list(args)
-			arg_list.pop(0)
-			arg_list.insert(0, argument)
-			args = tuple(arg_list)
-
-		if isinstance(argument, (np.ndarray, list, tuple)):
-			return func(*args, **kwargs)
-		elif np.isscalar(argument):
-			return alt_func(*args, **kwargs)
-		else:
-			return np.array([])
-
-	return _inner
-
-
-# Decorated sine function for both vectors and scalars.
-universalSin = universalDecorator(sinList, sinScalar)
+    return res
 
 
 ## TEST BEHAVIOUR
 
 temp_list = [3.2, 0.6, 1]
-print 'List: ', universalSin(temp_list)
-print 'Tuple: ', universalSin(tuple(temp_list))
+print 'List: ', sinList(temp_list)
+print 'Tuple: ', sinList(tuple(temp_list))
 
-print 'Numpy array: ', universalSin(np.array(temp_list))
+print 'Numpy array: ', sinList(np.array(temp_list))
 
 # Warning: Order of elements is not preserved in sets!
-print 'Set: ', universalSin(set(temp_list))
-print 'Frozenet: ', universalSin(frozenset(temp_list))
+print 'Set: ', sinList(set(temp_list))
+print 'Frozenet: ', sinList(frozenset(temp_list))
 
 
-print 'Scalar: ', universalSin(3.2)
-print 'Scalar: ', universalSin(0.6)
-print 'Scalar: ', universalSin(1)
+print 'Scalar: ', sinList(3.2)
+print 'Scalar: ', sinList(0.6)
+print 'Scalar: ', sinList(1)
 
-print 'None: ', universalSin(None)
+print 'None: ', sinList(None)
 
 # Warning, bool types are interpreted as scalars! True = 1, False = 0
-print 'Bool (True):', universalSin(True)
-print 'Bool (False):', universalSin(False)
+print 'Bool (True):', sinList(True)
+print 'Bool (False):', sinList(False)
 
