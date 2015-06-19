914f4f4bd1fbf6018e06f2c70cdd3b3833539444
dvida <denis.vida@gmail.com>
2015-06-19 12:17:01 +0200

    added an option to sample the time domain in regularly spaced intervals

--- fireballFitter/fireballFitter.py
+++ fireballFitter/fireballFitter.py
@@ -21,6 +21,11 @@ data_file = 'input.txt'
 # False = Use the 4-parameter fit for the whole flight path
 extended_ending = True
 
+# Sample the time domain at regular intervals
+time_domain_sample = True
+# Sample step (seconds)
+time_sample_step = 0.2
+
 
 from scipy.optimize import curve_fit, fsolve
 import matplotlib.pyplot as plt
@@ -98,12 +103,12 @@ def loadData(file_name):
         sigma_data = np.ones_like(t_data)
 
     # # Sort data by time
-
-    # data = np.vstack((t_data, s_data, h_data, sigma_data)).T
-    # # Sort by first row (time)
-    # data = data[data[:,0].argsort()]
-    # # Split back to individual arrays
-    # t_data, s_data, h_data, sigma_data = np.vsplit(data.T, 4)
+    data = np.vstack((t_data, s_data, h_data, sigma_data)).T
+    # Sort by first row (time)
+    data = data[data[:,0].argsort()]
+    # Split back to individual arrays
+    t_data, s_data, h_data, sigma_data = np.vsplit(data.T, 4)
+    t_data, s_data, h_data, sigma_data = t_data.T.ravel(), s_data.T.ravel(), h_data.T.ravel(), sigma_data.T.ravel()
     
 
     return t_data, s_data, h_data, sigma_data
@@ -166,7 +171,7 @@ def inflectionFinder(func2ndDerive, args1, funcExtend2ndDerive, args2, max_value
 
     # Look for sign change in the function's second derivative
     for i in domain:
-        
+        #print func2ndDerive(i, *args1) - funcExtend2ndDerive(i, *args2)
         if np.sign(func2ndDerive(i, *args1) - funcExtend2ndDerive(i, *args2)) != np.sign(func2ndDerive(i + step, *args1) - funcExtend2ndDerive(i + step, *args2)):
             # Return the mean value of the two points between which the change in the derivative sign has been detected
             return (2*i + step) / 2
@@ -203,12 +208,14 @@ def funcsIntersect(x, ycoeffs, y_extended):
     return intersections[-1]
 
 
-def fitData(t_data, y_data, sigma_data, velocity=False, int_point=None, coeff_name='x'):
+def fitData(t_data, y_data, sigma_data, sampled=True, step=0.2, velocity=False, int_point=None, coeff_name='x'):
     """ Fits the given data to the provided funtiones defined (func and funcExtend). 
 
+    sampled: If True, it will return the sampled time domain with <step> spaced samples, if False, it will use the original t_data time points
+    step: optional - used when sampled == True, defines the time spacing between each sample
     velocity: If False it returns the fit in the spatial space, if True it returns the fitted velocity
-    int_point: precalculated intersection point, default None
-    coef_name: name of the coeficient to be calculated (used for file saving)
+    int_point: Precalculated intersection point, default None
+    coef_name: Name of the coeficient to be calculated (used for file saving)
     """
 
     ## Fit y_data
@@ -221,6 +228,11 @@ def fitData(t_data, y_data, sigma_data, velocity=False, int_point=None, coeff_na
 
     # x_coeffs contains curve coeficients, p0 is the inital guess for the coeficients
     ycoeffs, pcov = curve_fit(func, xdata = t_data, ydata = y_data, sigma = sigma_data, p0=y_p0)
+
+    if sampled:
+        t_data = np.arange(0, max(t_data), step)
+        max_value = np.array([max(t_data)])
+        t_data = np.concatenate((t_data, max_value))
 
     if extended_ending:
 
@@ -283,14 +295,20 @@ def fitData(t_data, y_data, sigma_data, velocity=False, int_point=None, coeff_na
 # Load data from a file
 t_data, s_data, h_data, sigma_data = loadData(data_file)
 
-s_coeffs, intersection, s_fit_points, t_points = fitData(t_data, s_data, sigma_data, coeff_name = 's')
+s_coeffs, intersection, s_fit_points, t_points = fitData(t_data, s_data, sigma_data, sampled = time_domain_sample, step= time_sample_step, coeff_name = 's')
 
 print 'Curve coefficients for s:', s_coeffs
 
 # Save coefficients to a file
 #saveCoeffs(s_coeffs, 's_coeffs.txt')
 
-# Plot data
+## Plot data
+
+# Adjust calcaulted data points size, make it bigger when sampled
+fitted_points_size = 3
+
+if time_domain_sample:
+    fitted_points_size *= 2
 
 ## Plot s_data
 plt.subplot(3, 1, 1)
@@ -302,7 +320,7 @@ real_data = plt.scatter(t_data, s_data, s = 3, c = 'b', edgecolors='none')
 plt.ylabel('Realtive path (km)')
 
 # Plot fitted function points (red)
-fitted_data = plt.scatter(t_points, s_fit_points, s = 3, c = 'r', edgecolors='none')
+fitted_data = plt.scatter(t_points, s_fit_points, s = fitted_points_size, c = 'r', edgecolors='none')
 
 # Plot the intersection point
 if intersection:
@@ -322,7 +340,7 @@ plt.grid()
 
 ## Fit h_data
 
-h_coeffs, intersection, h_fit_points, t_points = fitData(t_data, h_data, sigma_data, int_point = intersection[0], coeff_name = 'h')
+h_coeffs, intersection, h_fit_points, t_points = fitData(t_data, h_data, sigma_data, sampled = time_domain_sample, step= time_sample_step, int_point = intersection[0], coeff_name = 'h')
 
 print 'Curve coefficients for h:', h_coeffs
 
@@ -333,7 +351,7 @@ print 'Curve coefficients for h:', h_coeffs
 real_data = plt.scatter(t_data, h_data, s = 3, c = 'b', edgecolors='none')
 
 # Plot fitted function points (red)
-fitted_data = plt.scatter(t_points, h_fit_points, s = 3, c = 'r', edgecolors='none')
+fitted_data = plt.scatter(t_points, h_fit_points, s = fitted_points_size, c = 'r', edgecolors='none')
 plt.ylabel('Height (km)')
 
 # Plot the intersection point
@@ -346,10 +364,9 @@ plt.subplot(3, 1, 3)
 plt.grid()
 
 # Calculate velocity from the fitted curve
-#v_data = funcDerive(t_data, *s_coeffs)
-s_coeffs, intersection, v_data, t_points = fitData(t_data, s_data, sigma_data, velocity = True, int_point = intersection[0])
+s_coeffs, intersection, v_data, t_points = fitData(t_data, s_data, sigma_data, velocity = True, sampled = time_domain_sample, step= time_sample_step, int_point = intersection[0])
 # Plot fitted function points (red)
-velocity_data = plt.scatter(t_points, v_data, s = 3, c = 'g', edgecolors='none')
+velocity_data = plt.scatter(t_points, v_data, s = fitted_points_size, c = 'g', edgecolors='none')
 plt.ylabel('Velocity (km/s)')
 
 #Plot the legend
@@ -365,7 +382,9 @@ plt.xlabel('Time (s)')
 # Export fitted data to a file
 exportDataMaria(t_points, h_fit_points, v_data)
 
-# Export raw height to a file
-exportDataMaria(t_points, h_data, v_data, file_name = "Alex_raw.txt")
+s_coeffs, intersection, v_data, t_points = fitData(t_data, s_data, sigma_data, velocity = True, sampled = False, int_point = intersection[0])
+
+# Export raw heights and original time domain velocities to a file
+exportDataMaria(t_data, h_data, v_data, file_name = "Alex_raw.txt")
 
 plt.show()
